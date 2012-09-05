ca5e8a21a847fe3bba6271ae75404e73a6626aba
billy <billy@debian.(none)>
2012-09-05 10:27:08 -0400

    euler problem one working

--- hello.cpp
+++ hello.cpp
@@ -2,7 +2,14 @@
 using std::cout;
 using std::endl;
 
+//euler 1
+
 int main() {
-	cout << "Hello World!" << endl;
-	return 0;
+	int sum = 0;
+	for (int i=1; i<1000; ++i) {
+		if (i%3==0 || i%5==0) {
+			sum += i;
+		}
+	}
+	cout << sum << endl;
 }
